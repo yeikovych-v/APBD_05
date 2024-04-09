e80787f145498424651dfb66014b541328688533
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-09 18:53:27 +0200

    Added Animal model class.

---
+++ APBD_05/model/animal/Animal.cs
@@ -0,0 +1,25 @@
+﻿namespace APBD_05.model;
+
+public class Animal(long id, string name, AnimalCategory animalCategory, decimal weight, FurColor furColor)
+{
+    public long Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public AnimalCategory AnimalCategory { get; set; } = animalCategory;
+    public decimal Weight { get; set; } = weight;
+    public FurColor FurColor { get; set; } = furColor;
+    
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
