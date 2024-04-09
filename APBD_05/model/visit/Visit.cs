beb3db02cc4d2a7ecf3c10eff74b2192d0a60719
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-09 19:01:52 +0200

    Added model for Visits.

---
+++ APBD_05/model/visit/Visit.cs
@@ -0,0 +1,11 @@
+﻿using APBD_05.model.animal;
+
+namespace APBD_05.model.visit;
+
+public class Visit(DateTime dateVisit, Animal animal, string description, decimal price)
+{
+    public DateTime DateVisit { get; set; } = dateVisit;
+    public Animal Animal { get; set; } = animal;
+    public string Description { get; set; } = description;
+    public decimal Price { get; set; } = price;
+}
