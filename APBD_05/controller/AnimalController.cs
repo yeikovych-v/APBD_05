e1bcf30289b8c63bb281797dc89d0d61961c1137
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-09 18:55:02 +0200

    Created AnimalController to handle operations with animals.

---
+++ APBD_05/controller/AnimalController.cs
@@ -0,0 +1,18 @@
+﻿using APBD_05.repository;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.AspNetCore.Http.Results;
+
+namespace APBD_05.controller;
+
+[Route("/animals")]
+[ApiController]
+public class AnimalController(IAnimalDatabase animalDb)
+{
+    [HttpGet]
+    public IResult GetAnimals()
+    {
+        return Ok(animalDb.FindAll());
+    }
+    
+    
+}
