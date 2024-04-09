cba1aa79e73d6a2aba64175c81544155e1ef6bfd
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-09 19:03:28 +0200

    Created animal db interface.

--- APBD_05/repository/IAnimalDatabase.cs
+++ APBD_05/repository/IAnimalDatabase.cs
@@ -1,6 +1,14 @@
-﻿namespace APBD_05.repository;
+﻿using APBD_05.model;
+using APBD_05.model.animal;
+
+namespace APBD_05.repository;
 
 public interface IAnimalDatabase
 {
-    
+    void AddAll(List<Animal> animals);
+    List<Animal> FindAll();
+    Animal? FindById(long id);
+    void Add(Animal animal);
+    bool EditById(long id, Animal animal);
+    bool DeleteById(long id);
 }
