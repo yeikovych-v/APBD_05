fd82cffaaf5e184d7847bd31707a316fa4366a6c
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-09 19:02:13 +0200

    Added In memory db implementation.

---
+++ APBD_05/repository/RuntimeVisitDb.cs
@@ -0,0 +1,31 @@
+﻿using APBD_05.model;
+using APBD_05.model.animal;
+using APBD_05.model.visit;
+
+namespace APBD_05.repository;
+
+public class RuntimeVisitDb : IVisitDatabase
+{
+
+    private static readonly Dictionary<Animal, List<Visit>> AnimalVisits = new();
+    
+    public void Add(Animal animal, Visit visit)
+    {
+        if (!AnimalVisits.ContainsKey(animal))
+        {
+            AnimalVisits.Add(animal, []);
+        }
+
+        AnimalVisits[animal].Add(visit);
+    }
+
+    public List<Visit> FindByAnimal(Animal animal)
+    {
+        return AnimalVisits.FirstOrDefault(pair => pair.Key == animal).Value;
+    }
+
+    public void AddAll(Animal animal, List<Visit> visits)
+    {
+        visits.ForEach(visit => Add(animal, visit));
+    }
+}
