e1ee10da00c32c84c3bbef4e6ed3dff53d08afc3
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-09 18:54:28 +0200

    Removed redundant methods.

--- APBD_05/model/animal/Animal.cs
+++ APBD_05/model/animal/Animal.cs
@@ -1,4 +1,4 @@
-﻿namespace APBD_05.model;
+﻿namespace APBD_05.model.animal;
 
 public class Animal(long id, string name, AnimalCategory animalCategory, decimal weight, FurColor furColor)
 {
@@ -7,19 +7,4 @@ public class Animal(long id, string name, AnimalCategory animalCategory, decimal
     public AnimalCategory AnimalCategory { get; set; } = animalCategory;
     public decimal Weight { get; set; } = weight;
     public FurColor FurColor { get; set; } = furColor;
-    
-    public override bool Equals(object? obj)
-    {
-        return base.Equals(obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return base.GetHashCode();
-    }
-
-    public override string ToString()
-    {
-        return base.ToString();
-    }
 }
